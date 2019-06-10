e9ce12ea63364a77a70ae719cad7f9f044cd7309
Max Fang <fangm8398@my.sduhsd.net>
2019-06-10 21:10:45 +0000

    sudoku

--- sudoku.php
+++ sudoku.php
@@ -12,14 +12,21 @@
 
     <?php
        // define variables and set to empty values
-       $arg1 = $arg2  = $output = $retc = "";
+       $arg1 = $arg2 = $arg3 = $arg4 = $arg5 = $arg6 = $arg7 = $arg8 = $arg9 = $string =  $output = $retc = "";
 
        if ($_SERVER["REQUEST_METHOD"] == "POST") {
          $arg1 = test_input($_POST["arg1"]);
          $arg2 = test_input($_POST["arg2"]);
-         exec(" /usr/lib/cgi-bin/sp2b/sudoku " . $arg1 . " " . $arg2, $output, $retc); 
-	}
-
+         $arg3 = test_input($_POST["arg3"]);
+         $arg4 = test_input($_POST["arg4"]);
+         $arg5 = test_input($_POST["arg5"]);
+         $arg6 = test_input($_POST["arg6"]);
+         $arg7 = test_input($_POST["arg7"]);
+         $arg8 = test_input($_POST["arg8"]);
+         $arg9 = test_input($_POST["arg9"]);
+         $string = $arg1 . " " . $arg2 . " " . $arg3 . " " . $arg4 . " " . $arg5 . " " . $arg6 . " " . $arg7 . " " .  $arg8 . " " . $arg9; 
+         exec("/usr/lib/cgi-bin/sp2b/sudoku \"" . $string . "\"", $output, $retc); 
+        }
        function test_input($data) {
          $data = trim($data);
          $data = stripslashes($data);
@@ -28,9 +35,17 @@
        }
     ?>
 
-    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-      Arg1: <input type="text" name="arg1"><br>
-      Arg2: <input type="text" name="arg2"><br>
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+        Arg1: <input type="text" name="arg1"><br>
+        Arg2: <input type="text" name="arg2"><br>
+        Arg3: <input type="text" name="arg3"><br>
+        Arg4: <input type="text" name="arg4"><br>
+        Arg5: <input type="text" name="arg5"><br>
+        Arg6: <input type="text" name="arg6"><br>
+        Arg7: <input type="text" name="arg7"><br>
+        Arg8: <input type="text" name="arg8"><br>
+        Arg9: <input type="text" name="arg9"><br>
+
       <br>
       <input type="submit" value="Go!">
     </form>
@@ -38,15 +53,9 @@
     <?php
        // only display if return code is numeric - i.e. exec has been called
        if (is_numeric($retc)) {
-         echo "<h2>Your Input:</h2>";
-         echo $arg1;
-         echo "<br>";
-         echo $arg2;
-         echo "<br>";
-       
-         echo "<h2>Program Output (an array):</h2>";
+         echo "<h2>Suduko Solution:</h2>";
          foreach ($output as $line) {
-           echo $line;
+           echo "<pre>$line</pre";
            echo "<br>";
          }
        
@@ -57,3 +66,4 @@
     
   </body>
 </html>
+
